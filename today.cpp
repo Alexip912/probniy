da43c03eb32aee4347c17b630eaf4c27121cd9ed
Mona Lisa <stzar12345@gmail.com>
2020-03-12 21:38:20 +0700

    Changed3 today.cpp

--- today.cpp
+++ today.cpp
@@ -1,1 +1,25 @@
-ffdsasgdsgaASFasyerh
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+float fe(float x,int n){
+  float pev=x;
+  if(n==0)
+  return 1;
+  if(n==1)
+  return x;
+  for(int i=1;i<abs(n);i++){
+  pev*=x;
+  }
+  if(n<0) pev=1./pev;
+  return pev;
+}
+int main(){
+	float a,b;
+	int c;
+	cin>>c>>a;
+	b=fe(c,a);
+	cout<<b<<endl;
+ system("PAUSE");
+return 0; 
+}
